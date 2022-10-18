57e07c87c51ac8746e19ab57ae44b43a4775b146
George Wiggin <george@sequest.io>
2022-10-18 12:38:01 +0100

    added model test for bank_account

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :bank_account do
-
+    name { 'Test Account' }
+    iban { 'RO66BACX0000001234567890' }
+    currency { 'USD' }
   end
 end
--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe BankAccount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:bank_account) { build(:bank_account) }
+
+  it 'name should be present ' do
+    bank_account.name = nil
+    expect(bank_account).to_not be_valid
+  end
 end
