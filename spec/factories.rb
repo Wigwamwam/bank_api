f3ced9dbe4d2a82f9a17c8ab3f2ae2977bc14c06
George Wiggin <george@sequest.io>
2022-10-18 12:00:29 +0100

    added Bank Account model

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :bank_account do
-    # name { Faker::Lorem.sentence }
-    # iban { Faker::Lorem.paragraph }
-    # currency { Faker::Lorem.paragraph }
+
   end
 end
