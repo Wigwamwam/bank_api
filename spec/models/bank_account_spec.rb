1de83b74828b8e08f0888881ad05d254e91d8e43
George Wiggin <george@sequest.io>
2022-10-18 14:33:39 +0100

    expanded on rspec models test

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -4,43 +4,46 @@ RSpec.describe BankAccount, type: :model do
 
   let(:bank_account) { build(:bank_account) }
 
-  it 'name should be present ' do
-    bank_account.name = nil
-    expect(bank_account).to_not be_valid
+  describe "--- Name -- " do
+    it 'name should be present ' do
+      bank_account.name = nil
+      expect(bank_account).to_not be_valid
+    end
+
+    it 'name should not exceed 100 characters' do
+      bank_account.name = 'a' * 101
+      expect(bank_account).to_not be_valid
+    end
   end
 
-  it 'name should not exceed 100 characters' do
-    bank_account.name = 'a' * 101
-    expect(bank_account).to_not be_valid
+  describe "--- Iban -- " do
+    it 'iban should be present ' do
+      bank_account.iban = nil
+      expect(bank_account).to_not be_valid
+    end
+
+    it 'iban should not exceed 34 characters ' do
+      bank_account.iban = 'a' * 34
+      expect(bank_account).to_not be_valid
+    end
+
+    it 'iban should only include letters and numbers' do
+      bank_account.iban = "!@£$%^&*()<>?:"
+      expect(bank_account).to_not be_valid
+    end
+
   end
 
-  # it 'name should not consist of 3 or less characters' do
-  #   bank_account.name = 'a' * 3
-  #   expect(bank_account).to_not be_valid
-  # end
+  describe "---Currency --" do
+    it 'currency should be present ' do
+      bank_account.currency = nil
+      expect(bank_account).to_not be_valid
+    end
 
-  it 'iban should be present ' do
-    bank_account.iban = nil
-    expect(bank_account).to_not be_valid
+    it 'currency should consists of GBP, EUR and USD ' do
+      bank_account.currency = "Dollars"
+      expect(bank_account).to_not be_valid
+    end
   end
 
-  it 'iban should not exceed 34 characters ' do
-    bank_account.iban = 'a' * 34
-    expect(bank_account).to_not be_valid
-  end
-
-  it 'iban should only include letters and numbers' do
-    bank_account.iban = "!@£$%^&*()<>?:"
-    expect(bank_account).to_not be_valid
-  end
-
-  it 'currency should be present ' do
-    bank_account.currency = nil
-    expect(bank_account).to_not be_valid
-  end
-
-  it 'currency should consists of GBP, EUR and USD ' do
-    bank_account.currency = "Dollars"
-    expect(bank_account).to_not be_valid
-  end
 end
