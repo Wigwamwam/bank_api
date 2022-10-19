3bdadc8d15d0f07806b7bb5aea4271ff59628549
George Wiggin <george@sequest.io>
2022-10-19 09:49:21 +0100

    bank_account routes - index, new, create, update, delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@
 
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :bank_accounts, only: [ :index, :new, :create, :update, :destroy]
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 FactoryBot.define do
   factory :bank_account do
-    name { Faker::Name.name }
+    name { "Test: #{ Faker::Name.name }" }
     iban { Faker::Bank.iban }
     currency { ['USD', 'GBP', 'EUR'].sample }
   end
