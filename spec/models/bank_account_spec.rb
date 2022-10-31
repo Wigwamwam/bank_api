e8a9f36ba29f4bf7e9486da184599b4dfe60fd0e
George Wiggin <george@sequest.io>
2022-10-31 11:15:41 +0000

    rubcop

--- spec/models/bank_account_spec.rb
+++ spec/models/bank_account_spec.rb
@@ -6,22 +6,22 @@ RSpec.describe BankAccount, type: :model do
   # let(:bank_account) { build(:bank_account) }
   context 'Model Attributes' do
     describe ' Name validations' do
-      it { should validate_presence_of(:name) }
-      it { should validate_length_of(:name).is_at_most(100) }
-      it { should_not allow_value(nil).for(:name) }
+      it { is_expected.to validate_presence_of(:name) }
+      it { is_expected.to validate_length_of(:name).is_at_most(100) }
+      it { is_expected.not_to allow_value(nil).for(:name) }
     end
 
     describe 'Iban validations ' do
-      it { should validate_presence_of(:iban) }
-      it { should validate_length_of(:iban).is_at_most(34) }
-      it { should_not allow_value(/[a-zA-Z0-9]\z/).for(:iban) }
-      it { should_not allow_value(nil).for(:iban) }
+      it { is_expected.to validate_presence_of(:iban) }
+      it { is_expected.to validate_length_of(:iban).is_at_most(34) }
+      it { is_expected.not_to allow_value(/[a-zA-Z0-9]\z/).for(:iban) }
+      it { is_expected.not_to allow_value(nil).for(:iban) }
     end
 
     describe 'Currency validations' do
-      it { should validate_presence_of(:currency) }
-      it { should validate_inclusion_of(:currency).in_array(%w[GBP EUR USD]) }
-      it { should_not allow_value(nil).for(:currency) }
+      it { is_expected.to validate_presence_of(:currency) }
+      it { is_expected.to validate_inclusion_of(:currency).in_array(%w[GBP EUR USD]) }
+      it { is_expected.not_to allow_value(nil).for(:currency) }
     end
   end
 end
